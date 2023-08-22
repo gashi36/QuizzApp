32b9d328fc1032a5fb174c2e3ae768edbcad3f51
Edon Gashi <egashi630@gmail.com>
2023-08-22 11:47:53 +0200

    login finnished\

--- interface/src/components/Login.js
+++ interface/src/components/Login.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
-import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
-import Center from './Center'
-import useForm from '../hooks/useForm'
+import React from 'react';
+import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import Center from './Center';
+import useForm from '../hooks/useForm';
 
 const getFreshModel = () => ({
     name: '',
     email: ''
-})
+});
+
 const Login = () => {
-    return {
-        values,
-        setValues,
-        errors,
-        setErrors,
-        handleInputChange
-    } = useForm(getFreshModel)
-    const { } = useForm();
+    const { values, setValues, errors, setErrors, handleInputChange } = useForm(getFreshModel);
+
+    const login = e => {
+        e.preventDefault();
+
+        if (validate()) {
+            console.log("Form is valid. Logging in...");
+            // Add your login logic here
+            // You can access the entered values through the 'values' object
+        } else {
+            console.log("Form is not valid. Please correct errors.");
+        }
+    };
+
+    const validate = () => {
+        let temp = {};
+        temp.email = /\S+@\S+\.\S+/.test(values.email) ? "" : "Email is not valid.";
+        temp.name = values.name !== "" ? "" : "This field is required.";
+        setErrors(temp);
+        return Object.values(temp).every(x => x === "");
+    };
+
     return (
         <Center>
             <Card sx={{ width: 400 }}>
@@ -27,30 +42,34 @@ const Login = () => {
                             width: '90%'
                         }
                     }}>
-                        <form noValidate>
+                        <form noValidate autoComplete='off' onSubmit={login}>
                             <TextField
                                 label="Email"
                                 name="email"
                                 value={values.email}
                                 onChange={handleInputChange}
-                                variant='outlined' />
+                                variant='outlined'
+                                {...(errors.email && { error: true, helperText: errors.email })}
+                            />
                             <TextField
                                 label="Name"
                                 name="name"
-                                variant='outlined' />
+                                value={values.name}
+                                onChange={handleInputChange}
+                                variant='outlined'
+                                {...(errors.name && { error: true, helperText: errors.name })}
+                            />
                             <Button
                                 type='submit'
                                 variant='contained'
                                 size='large'
                                 sx={{ width: '90%' }}>Start</Button>
-
-
                         </form>
                     </Box>
                 </CardContent>
             </Card>
         </Center>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
--- interface/src/hooks/useForm.js
+++ interface/src/hooks/useForm.js
@@ -1,24 +1,22 @@
-
-import { useState } from 'react'
+import { useState } from 'react';
 
 export default function useForm(getFreshModelObject) {
-
     const [values, setValues] = useState(getFreshModelObject());
     const [errors, setErrors] = useState({});
 
     const handleInputChange = e => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
+        const { name, value } = e.target;
+        setValues(prevValues => ({
+            ...prevValues,
             [name]: value
-        })
-    }
+        }));
+    };
+
     return {
         values,
         setValues,
         errors,
         setErrors,
         handleInputChange
-    }
-
+    };
 }
