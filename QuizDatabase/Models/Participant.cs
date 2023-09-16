7a699519d43de1f79b13c4d209eed9ef61f4a2cf
Edon Gashi <egashi630@gmail.com>
2023-09-16 12:27:12 +0200

    last commit

---
+++ QuizDatabase/Models/Participant.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizDatabase.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int ParticipantId { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public required string Email { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public required string Name { get; set; }
+       
+        public int Score { get; set; }
+       
+        public int TimeTaken { get; set; }
+
+
+    }
+
+    public class ParticipantResult
+    { 
+        public int ParticipantId { get; set; }
+        public int Score {get; set; }
+        public int TimeTaken { get; set; } 
+    
+    }
+
+
+}
